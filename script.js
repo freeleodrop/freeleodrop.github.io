0cea8c382b5a4c5dc613f081a1ee3cbe30f3ea47
freeleodrop <freemeleo27@gmail.com>
2024-10-29 15:46:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const products = [
+  { name: 'AirPods', category: 'AirPods' },
+  { name: 'AirPods Pro', category: 'AirPods' },
+  { name: 'Apple Gift Card', category: 'Gift Cards' },
+  { name: 'MacBook Stand', category: 'MacBook Accessories' }
+];
 
+function searchProducts() {
+  const searchBar = document.getElementById('searchBar');
+  const query = search
